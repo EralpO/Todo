9b5eabb3bf86ef89f1e44f69dfb94c631d1d3060
EralpO <eralponay10@gmail.com>
2020-07-17 22:54:15 +0300

    initial commit

---
+++ Todo.Web/TagHelpers/Section.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Web.TagHelpers
+{
+    [HtmlTargetElement("sectionhelper")]
+    public class Section : TagHelper
+    {
+        public string Data { get; set; } = "Eralp";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            
+        var stringBuilder = new StringBuilder();
+            stringBuilder.Append("<b> Merhabalar </b>" +Data);
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+            base.Process(context, output);
+        }
+       
+
+    }
+}
